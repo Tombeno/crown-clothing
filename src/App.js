2ed18e6793235845f2afc023a0d846bd12361576
Tomm <aritonang31031996@gmail.com>
2019-08-13 12:34:28 +0700

    tambah halaman shop, collection item dan preview

--- src/App.js
+++ src/App.js
@@ -4,19 +4,16 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/toko/toko.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>Bagian Topi</h1>
-  </div>
-);
+
 
 function App() {
   return (
     <div>
      <Switch>
        <Route exact path='/' component={HomePage} />
-       <Route exact path='/topi' component={HatsPage} />
+       <Route exact path='/toko' component={ShopPage} />
      </Switch>
     </div>
   );
